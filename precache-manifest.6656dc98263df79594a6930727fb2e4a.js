c5e2b782bf05e2a62abd00142a0ee7feb044d242
Volodymyr Viter <mad.tvido@gmail.com>
2021-11-09 23:53:24 +0200

    Updates

---
+++ precache-manifest.6656dc98263df79594a6930727fb2e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5281cc5654dcc10bd9f",
+    "url": "/css/app.a3fc13ce.css"
+  },
+  {
+    "revision": "eb185d96d8cd3439c120b2fd046740a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5281cc5654dcc10bd9f",
+    "url": "/js/app.90a050c1.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
